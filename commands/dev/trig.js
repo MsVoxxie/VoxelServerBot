bfb6fd2182cc87655492bf8965a6ec470a5fce7b
MsVoxxie <odysseylove222@gmail.com>
2024-09-09 00:30:47 -0400

    Refactor trig.js command to remove unused code

--- commands/dev/trig.js
+++ commands/dev/trig.js
@@ -1,23 +1,9 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { fetchTriggerTaskId, fetchTaskId } = require('../../functions/ampAPI/apiFunctions');
 module.exports = {
 	data: new SlashCommandBuilder().setName('triggers').setDescription('Test the trigger functions'),
 	options: {
 		devOnly: true,
 		disabled: false,
 	},
-	async execute(client, interaction, settings) {
-		const server = '68574cfd-53da-4d0b-8558-d00b791b3bc7';
-
-		const test = await fetchTriggerTaskId(server, 'A player sends a chat message', 'MakePOSTRequest');
-
-		console.log(test);
-		
-		return;
-
-		const triggerID = await fetchTaskId(server, 'MakePOSTRequest');
-		await serverInstance.Core.DeleteTaskAsync(chatMessageTrigger.Id, postRequestEvent.Id).then((t) => console.log(t));
-
-		console.log(triggerID);
-	},
+	async execute(client, interaction, settings) {},
 };
