e72298a48a6b770e28b11d5a22b650a8443fa95b
MsVoxxie <odysseylove222@gmail.com>
2024-09-26 06:17:37 -0400

    Fix

--- events/bot_events/03 oneMinute.js
+++ events/bot_events/03 oneMinute.js
@@ -10,31 +10,27 @@ module.exports = {
 		const { totalInstances, oldInstances } = client;
 
 		// Check if the instance count has changed and find any added instances
-		if (totalInstances !== instanceCount) {
-			const addedInstances = allInstances.filter((i) => !oldInstances.some((old) => old.instanceId === i.instanceId));
+		const addedInstances = allInstances.filter((i) => !oldInstances.some((old) => old.instanceId === i.instanceId));
 
-			// If there are added instances, log them
-			if (addedInstances.length > 0) {
-				addedInstances.forEach((i) => {
-					// Emit an event for the added instance
-					client.emit('serverInstanceCreated', i);
-					logger.info(`Added instance: ${i.instanceName} (${i.instanceFriendlyName})`);
-				});
-			}
+		// If there are added instances, log them
+		if (addedInstances.length > 0) {
+			addedInstances.forEach((i) => {
+				// Emit an event for the added instance
+				client.emit('serverInstanceCreated', i);
+				logger.info(`Added instance: ${i.instanceName} (${i.instanceFriendlyName})`);
+			});
 		}
 
 		// Check if the instance count has changed and find any removed instances
-		if (totalInstances !== instanceCount) {
-			const removedInstances = oldInstances.filter((i) => !allInstances.some((newI) => newI.instanceId === i.instanceId));
+		const removedInstances = oldInstances.filter((i) => !allInstances.some((newI) => newI.instanceId === i.instanceId));
 
-			// If there are removed instances, log them
-			if (removedInstances.length > 0) {
-				removedInstances.forEach((i) => {
-					// Emit an event for the removed instance
-					client.emit('serverInstanceDeleted', i);
-					logger.info(`Removed instance: ${i.instanceName} (${i.instanceFriendlyName})`);
-				});
-			}
+		// If there are removed instances, log them
+		if (removedInstances.length > 0) {
+			removedInstances.forEach((i) => {
+				// Emit an event for the removed instance
+				client.emit('serverInstanceDeleted', i);
+				logger.info(`Removed instance: ${i.instanceName} (${i.instanceFriendlyName})`);
+			});
 		}
 
 		// Update the client with the new instance count
