8f6609a6710fae7feeccb87b7c7f9d23c71cd98b
MsVoxxie <odysseylove222@gmail.com>
2024-09-30 07:39:10 -0400

    Refactor serverToDiscord.js to remove duplicate message check

--- events/chatLink/serverToDiscord.js
+++ events/chatLink/serverToDiscord.js
@@ -6,10 +6,8 @@ module.exports = {
 	async execute(client, data) {
 		// Check for Prestart and duplicate messages
 		if (data.MESSAGE === 'Server PreStart') return;
-		if (client.lastReceivedChat === data.MESSAGE) return;
 
 		// Send webhook
 		await serverLink(data.USER, data.MESSAGE, data.INSTANCE);
-		client.lastReceivedChat = data.MESSAGE;
 	},
 };
