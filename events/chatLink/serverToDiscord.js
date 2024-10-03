7d32adff5262fefc773c70548cc170ed486b6d01
MsVoxxie <odysseylove222@gmail.com>
2024-10-02 21:06:35 -0400

    bah

--- events/chatLink/serverToDiscord.js
+++ events/chatLink/serverToDiscord.js
@@ -4,9 +4,6 @@ module.exports = {
 	name: 'receivedChat',
 	runType: 'infinity',
 	async execute(client, data) {
-		// Check for Prestart and duplicate messages
-		if (data.MESSAGE === 'Server PreStart') return;
-
 		// Send webhook
 		await serverLink(data.USER, data.MESSAGE, data.INSTANCE);
 	},
