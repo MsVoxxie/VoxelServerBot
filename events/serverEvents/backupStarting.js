57066663ef9d5badeb41b1e4458c7e331fe45430
MsVoxxie <odysseylove222@gmail.com>
2025-01-12 13:55:08 -0500

    Add backup event handlers for starting and completion notifications

---
+++ events/serverEvents/backupStarting.js
@@ -0,0 +1,13 @@
+const { serverLink } = require('../../functions/helpers/messageDiscord');
+
+module.exports = {
+	name: 'backupStarting',
+	runType: 'infinity',
+	async execute(client, data) {
+		// Split the data into variables
+		const { USER, INSTANCE, MESSAGE } = data;
+
+		// Send off the message to Discord
+		await serverLink(USER, MESSAGE, INSTANCE);
+	},
+};
