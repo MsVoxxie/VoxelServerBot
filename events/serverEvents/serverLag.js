4f226f54c27ecef0e6273e9d832c2c8334ab7d03
MsVoxxie <odysseylove222@gmail.com>
2025-01-13 19:07:15 -0500

    Removed log

--- events/serverEvents/serverLag.js
+++ events/serverEvents/serverLag.js
@@ -1,5 +1,12 @@
 const { serverLink } = require('../../functions/helpers/messageDiscord');
 
+const lastSent = new Map();
+
+// Clear the last sent time every 15 minutes
+setInterval(() => {
+	lastSent.clear();
+}, 900000);
+
 module.exports = {
 	name: 'serverLag',
 	runType: 'infinity',
@@ -7,7 +14,12 @@ module.exports = {
 		// Split the data into variables
 		const { USER, INSTANCE, MESSAGE } = data;
 
+		// Only allow this to be sent every 5 minutes
+		if (lastSent.has(INSTANCE) && Date.now() - lastSent.get(INSTANCE) < 300000) return;
 		// Send off the message to Discord
 		await serverLink(USER, MESSAGE, INSTANCE, 'Alert', true);
+
+		// Update the last sent time
+		lastSent.set(INSTANCE, Date.now());
 	},
 };
