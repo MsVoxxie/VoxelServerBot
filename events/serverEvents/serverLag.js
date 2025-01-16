5f6342cce13acbcf37eafa1e620990eebe62760b
MsVoxxie <odysseylove222@gmail.com>
2025-01-16 00:15:24 -0500

    lower case oopsie

--- events/serverEvents/serverLag.js
+++ events/serverEvents/serverLag.js
@@ -17,7 +17,7 @@ module.exports = {
 		// Only allow this to be sent every 5 minutes
 		if (lastSent.has(INSTANCE) && Date.now() - lastSent.get(INSTANCE) < 300000) return;
 		// Send off the message to Discord
-		await serverLink(USER, MESSAGE, INSTANCE, 'Alert', true);
+		await serverLink(USER, MESSAGE, INSTANCE, 'alert', true);
 
 		// Update the last sent time
 		lastSent.set(INSTANCE, Date.now());
