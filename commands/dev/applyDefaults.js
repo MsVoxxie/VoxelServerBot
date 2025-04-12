7d6c93d45ff21b4a82179a1c60db86494f9b8055
MsVoxxie <odysseylove222@gmail.com>
2025-04-11 20:58:14 -0400

    Add applyDefaults command to reapply default settings to instances

---
+++ commands/dev/applyDefaults.js
@@ -0,0 +1,53 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { mainAPI, instanceAPI } = require('../../functions/ampAPI/apiFunctions');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('apply_defaults')
+		.setDescription('Reapply default settings to instances')
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+	options: {
+		devOnly: true,
+		disabled: false,
+	},
+	async execute(client, interaction, settings) {
+		// Defer the reply
+		await interaction.deferReply();
+
+		// Get the ADS API
+		const API = await mainAPI();
+
+		// Get all the instances
+		const getInstances = await API.ADSModule.GetInstancesAsync();
+		const instanceIDs = getInstances[0].AvailableInstances.filter((instance) => instance.InstanceName !== 'ADS01' && instance.Running !== false)
+			.map((instance) => ({ name: instance.InstanceName, id: instance.InstanceID }))
+			.sort((a, b) => a.name.localeCompare(b.name));
+
+		// Get the default settings from the ADS module
+		const getInstanceDefaults = await API.Core.GetConfigAsync('ADSModule.Defaults.DefaultSettings');
+		const instanceDefaults = getInstanceDefaults.CurrentValue;
+
+		// Sort settings by key name
+		const sortedDefaults = Object.fromEntries(Object.entries(instanceDefaults).sort(([keyA], [keyB]) => keyA.localeCompare(keyB)));
+
+		// Apply the settings to each instance
+		for await (const instance of instanceIDs) {
+			console.log(`Applying settings to ${instance.name} - (${instance.id})...`);
+
+			try {
+				const instAPI = await instanceAPI(instance.id);
+				await instAPI.Core.SetConfigsAsync(sortedDefaults);
+			} catch (error) {
+				console.log(`❌ Failed to apply settings to ${instance.name} - (${instance.id})...`);
+				continue;
+			} finally {
+				console.log(`✅ Settings applied to ${instance.name} - (${instance.id})...`);
+			}
+		}
+		console.log('All instances processed.');
+
+		// Send a response to the interaction
+		await interaction.followUp({
+			content: 'Default settings have been reapplied to all online instances.',
+		});
+	},
+};
--- commands/dev/trig.js
+++ commands/dev/trig.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } = require('discord.js');
-
+const { mainAPI, instanceAPI } = require('../../functions/ampAPI/apiFunctions');
 module.exports = {
 	data: new SlashCommandBuilder().setName('triggers').setDescription('Test the trigger functions').setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	options: {
