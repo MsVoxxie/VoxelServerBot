8f7d1d31ce482e071a925f03303a2f7379934907
MsVoxxie <odysseylove222@gmail.com>
2025-04-24 20:52:15 -0400

    fixed flex

--- events/serverEvents/playerAdvancement.js
+++ events/serverEvents/playerAdvancement.js
@@ -6,7 +6,12 @@ module.exports = {
 	runType: 'infinity',
 	async execute(client, data) {
 		// Split the data into variables
-		const { USER, INSTANCE, MESSAGE } = data;
+		let { USER, INSTANCE, MESSAGE } = data;
+
+		MESSAGE = MESSAGE.replace(
+			'has made the advancement **Monster Hunter**',
+			'has made the advancement **[Monster Hunter](https://vsb.voxxie.me/v1/static/images/monster_hunter.jpg)**'
+		);
 
 		// Send off the message to Discord
 		queueTask(INSTANCE, serverLink, USER, MESSAGE, INSTANCE);
